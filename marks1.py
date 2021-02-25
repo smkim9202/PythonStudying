0000dba108c11e58faf0c31477d1b57f02a857db
개발슝이 <smkim9202@gmail.com>
2021-02-25 22:38:07 +0900

    Add files via upload

--- marks1.py
+++ marks1.py
@@ -1,10 +1,10 @@
 # marks1.py
 
-marks = [90,25,67,45,80]
+marks = [90, 25, 67, 45, 80]
 number = 0
-for i in marks:
+for mark in marks:
     number += 1
-    if i >= 60:
-        print("%d 학생 합격입니다."%number)
+    if mark >= 60:
+        print("%d 학생 합격입니다." % number)
     else:
-        print("%d 학생 불합격입니다." %number)
+        print("%d 학생 불합격입니다." % number)
--- marks2.py
+++ marks2.py
@@ -1,8 +1,8 @@
 # marks2.py
+
 marks = [90, 25, 67, 45, 80]
 number = 0
 for mark in marks:
-    number = number + 1
+    number += 1
     if mark < 60: continue
-    print("%d번 학생 축하합니다. 합격입니다." %number)
-    
+    print("%d 학생 축하합니다. 합격입니다." % number)
