97788e9283385a3db629095caba3c656c02ef5da
개발슝이 <smkim9202@gmail.com>
2021-03-03 23:56:08 +0900

    Add files via upload

---
+++ 04Q3.py
@@ -0,0 +1,10 @@
+# 04장 연습문제 Q3
+
+def numadd():
+    input1 = int(input("첫번째 숫자를 입력하세요: "))
+    input2 = int(input("두번째 숫자를 입력하세요: "))
+    total = input1 + input2
+    return print("두 수의 합은 %s 입니다." %total)
+
+
+numadd()
