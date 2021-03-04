6b15aa95f992a8d34c0ab614dc05249cf262bfd8
개발슝이 <smkim9202@gmail.com>
2021-03-04 20:05:10 +0900

    Add files via upload

---
+++ 04Q4.py
@@ -0,0 +1,6 @@
+# 04장 연습문제 Q4
+
+print("You" "need" "python")  #Youneedpython
+print("You"+"need"+"python")  #Youneedpython
+print("You", "need", "python")  #You need python
+print("".join(["You", "need", "python"]))  #Youneedpython
