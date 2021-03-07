7c1a6d7eeff247104608f14a91985edbf522eab9
개발슝이 <smkim9202@gmail.com>
2021-03-07 22:07:02 +0900

    Add files via upload

---
+++ 05Q2.py
@@ -0,0 +1,20 @@
+# 05장 연습문제 Q2
+
+class Calculator:
+    def __init__(self):
+        self.value = 0
+
+    def add(self, val):
+        self.value += val
+
+class MaxLimitCalculator(Calculator):
+    def add(self, val):
+        self.value += val
+        if self.value > 100:
+            self.value = 100
+
+cal = MaxLimitCalculator()
+cal.add(50)
+cal.add(100)
+
+print(cal.value)
